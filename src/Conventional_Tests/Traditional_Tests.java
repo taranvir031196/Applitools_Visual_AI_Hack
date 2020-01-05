a32b1ad9d9f36681f9dd2d33a25a2ddce6100640
TARANVIR SINGH SAINI <taranvir.554@gmail.com>
2020-01-06 04:40:12 +0530

    Commited the structure

---
+++ src/Conventional_Tests/Traditional_Tests.java
@@ -0,0 +1,10 @@
+package Conventional_tests
+
+public class Traditional_Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Visual_AI/Visual_AI_Tests.java
@@ -0,0 +1,12 @@
+package Visual_AI
+
+
+
+public class Visual_AI_Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
